bb836d87bdfc12d473317fe76af10ac59294f683
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2025-11-08 15:30:18 +0200

    deploy chnages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1f27d3c898ae07b1"; }
+/******/ 		__webpack_require__.h = function() { return "24ddc5a359316aeb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
