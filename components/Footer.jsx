45a65406907a0887d2697dbb3772bd19faf0273d
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2025-11-08 11:22:38 +0200

    deploy chnages

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -18,9 +18,11 @@ const Footer = () => {
           <div className={styles.card}>
             <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
             <p className={styles.text}>
-              Al-Teraa Street
-              <br /> in front of Toshka Gate
-              <br />University neighborhood
+              1654 R. Don Road #304.
+              <br /> NewYork, 85022
+              <br /> (602) 867-1010
+              <br /> Mansoura
+              <br /> Giza
             </p>
           </div>
           <div className={styles.card}>
